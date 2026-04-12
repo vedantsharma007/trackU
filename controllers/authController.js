1e56f99eaee74e0fc8bd1c092bd702ca82a83170
Vedant Sharma <sharmavedant644@gmail.com>
2026-04-12 12:04:21 +0530

    adding authentication to the application and testing it by register ,login and register with used email

---
+++ controllers/authController.js
@@ -0,0 +1,71 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// LOGIN
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && await bcrypt.compare(password, user.password)) {
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+      });
+
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token
+      });
+
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
+
+
+
 dotenv.config();
 connectDB();
 
@@ -22,3 +27,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use("/api/auth", authRoutes);
