bd09acc0a206b6a0b8472e264f6a0ed71ced7d26
Vedant Sharma <sharmavedant644@gmail.com>
2026-04-18 20:06:57 +0530

    adding security layer to the system so that it will safe from too many requests and exploitation

--- server.js
+++ server.js
@@ -6,14 +6,32 @@ const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const errorHandler = require("./middleware/errorMiddleware");
 
+// 🔐 NEW IMPORTS
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
 dotenv.config();
 connectDB();
 
 const app = express();
 
+// 🔐 SECURITY MIDDLEWARE
+
+// 1. Helmet → secure HTTP headers
+app.use(helmet());
+
+// 2. Rate limiting → prevent spam/abuse
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 10, // max 100 requests per IP
+  message: "Too many requests, try again later"
+});
+app.use(limiter);
+
+// Body parser
 app.use(express.json());
 
-// ROUTES (before listen)
+// ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
@@ -21,7 +39,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// ERROR HANDLER (LAST)
+// ERROR HANDLER
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
