d6e9f6ec093a944acf992e5ac401d8f40b541c8d
Vedant Sharma <sharmavedant644@gmail.com>
2026-05-04 11:17:08 +0530

    change port no. according to vite

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 // ✅ CORS (FIRST)
 // ==================
 app.use(cors({
-  origin: "http://localhost:3000", // frontend
+  origin: "http://localhost:5173", // frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
